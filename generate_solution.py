e12d37d26376148aeb281daef93bf12f498b603d
Seb Vince <sebvince@amd.com>
2025-06-30 08:36:01 +0000

    set matmul type as parameter

--- generate_solution.py
+++ generate_solution.py
@@ -1,7 +1,7 @@
 import numpy as np
 import sys
 
-def generate_calls_file(m: int, n: int, k: int, output_file: str):  
+def generate_calls_file(m: int, n: int, k: int, dtype: str, output_file: str):  
     content = f"""builtin.module @calls attributes {{  
     
 }} {{  
@@ -18,12 +18,12 @@ func.func @matmul() attributes {{
   %device = hal.devices.get %device_index : !hal.device  
   %lhs_dim0 = arith.constant {m} : i64  
   %lhs_dim1 = arith.constant {k} : i64  
-  %lhs_element_type = hal.element_type<f8E4M3FNUZ> : i32  
+  %lhs_element_type = hal.element_type<{dtype}> : i32  
   %lhs_seed = arith.constant 5 : i32  
   %lhs = call @matmul_test.generate_random_matrix(%device, %lhs_dim0, %lhs_dim1, %lhs_element_type, %lhs_seed) : (!hal.device, i64, i64, i32, i32) -> !hal.buffer_view  
   %rhs_dim0 = arith.constant {n} : i64  
   %rhs_dim1 = arith.constant {k} : i64  
-  %rhs_element_type = hal.element_type<f8E4M3FNUZ> : i32  
+  %rhs_element_type = hal.element_type<{dtype}> : i32  
   %rhs_seed = arith.constant 6 : i32  
   %rhs = call @matmul_test.generate_random_matrix(%device, %rhs_dim0, %rhs_dim1, %rhs_element_type, %rhs_seed) : (!hal.device, i64, i64, i32, i32) -> !hal.buffer_view  
   %acc = util.null : !hal.buffer_view  
@@ -41,13 +41,13 @@ func.func @matmul() attributes {{
         f.write(content)  
 
 
-def generate_matmul_file(m: int, n: int, k: int, output_file: str):  
-    content = f"""func.func @matmul(%lhs: tensor<?x{k}xf8E4M3FNUZ>, %rhs: tensor<{n}x{k}xf8E4M3FNUZ>) -> tensor<?x{n}xf32> {{
+def generate_matmul_file(m: int, n: int, k: int, dtype : str, output_file: str):  
+    content = f"""func.func @matmul(%lhs: tensor<?x{k}x{dtype}>, %rhs: tensor<{n}x{k}x{dtype}>) -> tensor<?x{n}xf32> {{
   %c0 = arith.constant 0 : index
   %c256 = arith.constant 256 : index
-  %m = tensor.dim %lhs, %c0 : tensor<?x{k}xf8E4M3FNUZ>
+  %m = tensor.dim %lhs, %c0 : tensor<?x{k}x{dtype}>
   %m_outer = arith.divsi %m, %c256 : index
-  %lhs_expanded = tensor.expand_shape %lhs [[0, 1], [2]] output_shape [%m_outer, 256, {k}] : tensor<?x{k}xf8E4M3FNUZ> into tensor<?x256x{k}xf8E4M3FNUZ>
+  %lhs_expanded = tensor.expand_shape %lhs [[0, 1], [2]] output_shape [%m_outer, 256, {k}] : tensor<?x{k}x{dtype}> into tensor<?x256x{k}x{dtype}>
   %init_acc = tensor.empty(%m_outer) : tensor<?x256x{n}xf32>
   %c0_acc_type = arith.constant 0.0: f32 
   %acc = linalg.fill ins(%c0_acc_type : f32) outs(%init_acc : tensor<?x256x{n}xf32>) -> tensor<?x256x{n}xf32>
@@ -59,12 +59,12 @@ def generate_matmul_file(m: int, n: int, k: int, output_file: str):
     ], iterator_types = [
       "parallel", "parallel", "parallel", "reduction"
     ]
-  }} ins(%lhs_expanded, %rhs : tensor<?x256x{k}xf8E4M3FNUZ>, tensor<{n}x{k}xf8E4M3FNUZ>)
+  }} ins(%lhs_expanded, %rhs : tensor<?x256x{k}x{dtype}>, tensor<{n}x{k}x{dtype}>)
     outs(%acc : tensor<?x256x{n}xf32>)
   {{
-  ^bb0(%lhs_val: f8E4M3FNUZ, %rhs_val: f8E4M3FNUZ, %out: f32):
-    %56 = arith.extf %lhs_val : f8E4M3FNUZ to f32
-    %57 = arith.extf %rhs_val : f8E4M3FNUZ to f32
+  ^bb0(%lhs_val: {dtype}, %rhs_val: {dtype}, %out: f32):
+    %56 = arith.extf %lhs_val : {dtype} to f32
+    %57 = arith.extf %rhs_val : {dtype} to f32
     %58 = arith.mulf %56, %57 : f32
     %59 = arith.addf %out, %58 : f32
     linalg.yield %59 : f32
@@ -75,23 +75,34 @@ def generate_matmul_file(m: int, n: int, k: int, output_file: str):
     with open(output_file, "w") as f:  
         f.write(content)  
 
+
 if __name__ == "__main__":  
-    if len(sys.argv) != 4:  
-        print(f"Usage: {sys.argv[0]} <m> <n> <k>")  
+    if len(sys.argv) != 5:  
+        print(f"Usage: {sys.argv[0]} <m> <n> <k> <type> ")  
         sys.exit(1)  
   
     m = int(sys.argv[1])  
     n = int(sys.argv[2])  
-    k = int(sys.argv[3])  
+    k = int(sys.argv[3])
+    type_in = sys.argv[4]
+
+    torch_type = np.uint16
+    n_bits = 16
+    dtype = type_in
+
+    if type_in == "f8":
+      n_bits = 8
+      torch_type = np.uint8
+      dtype = "f8E4M3FNUZ"
+
 
     # generate mlir files
-    generate_calls_file(m,n,k, "calls.mlir")  
-    generate_matmul_file(m,n,k, "matmul.mlir")  
+    generate_calls_file(m,n,k, dtype,  "calls.mlir")  
+    generate_matmul_file(m,n,k, dtype, "matmul.mlir")  
 
     # generate random data
-
-    l = np.random.randint(low=0, high=255, size=(m, k), dtype=np.uint8)
-    r = np.random.randint(low=0, high=255, size=(k, n), dtype=np.uint8)
+    l = np.random.randint(low=0, high=(1<<n_bits-1), size=(m, k), dtype=torch_type)
+    r = np.random.randint(low=0, high=(1<<n_bits-1), size=(k, n), dtype=torch_type)
 
     l.tofile("lhs.bin")
     r.tofile("rhs.bin")
--- test_matmul.sh
+++ test_matmul.sh
@@ -2,21 +2,29 @@
 
 # prefill_bs4$async_dispatch_21_matmul_like_Dx14336x4096_f8E4M3FNUZxf8E4M3FNUZxf32.kd
 # prefill_bs4$async_dispatch_22_matmul_like_Dx14336x4096_f8E4M3FNUZxf8E4M3FNUZxf32.kd
-
-
 # prefill_bs4$async_dispatch_645_matmul_like_Dx128256x4096_bf16xbf16xf32.kd
-
 # prefill_bs4$async_dispatch_23_matmul_like_Dx4096x14336_f8E4M3FNUZxf8E4M3FNUZxf32.kd
 
 set -eux
 
 ninja -C $HOME/iree-build iree-compile iree-e2e-matmul-test
 
-M=16384
-N=4096
-K=32768
+#llama
+# M=8192
+# N=14336
+# K=4096
+# TYPE=f8E4M3FNUZ
 
-python3 ./generate_solution.py ${M}  ${N}  ${K}
+M=16384
+N=8192
+K=4096
+TYPE=bf16
+
+# M=8192
+# N=4096
+# K=14336
+#f8E4M3FNUZ
+python3 ./generate_solution.py ${M}  ${N}  ${K} ${TYPE}
 
 $HOME/iree-build/tools/iree-compile calls.mlir \
     --iree-hip-target=gfx942 \
@@ -28,22 +36,20 @@ $HOME/iree-build/tools/iree-compile matmul.mlir \
     --iree-hal-target-backends=rocm \
     --mlir-disable-threading \
     --iree-codegen-enable-default-tuning-specs=true \
-    --iree-hal-dump-executable-binaries-to .  \
-    --iree-hal-dump-executable-sources-to . \
-    --iree-hal-dump-executable-intermediates-to . \
-    --iree-hal-dump-executable-files-to . \
+    --iree-hal-dump-executable-intermediates-to files \
+    --iree-hal-dump-executable-files-to files \
     --iree-opt-level=O3 \
     -o tmp/dispatch.vmfb \
     # --mlir-print-ir-after-all \
     # --mlir-disable-threading 2> out.mlir
 
 
-# $HOME/iree-build/tools/testing/e2e/iree-e2e-matmul-test \
-#   --device=hip \
-#   --module=tmp/dispatch.vmfb \
-#   --module=tmp/calls.vmfb \
-#   --acceptable_fp_delta=1e-02
-PROFILER="rocprofv3 --att --"
+$HOME/iree-build/tools/testing/e2e/iree-e2e-matmul-test \
+  --device=hip \
+  --module=tmp/dispatch.vmfb \
+  --module=tmp/calls.vmfb \
+  --acceptable_fp_delta=1e-02
+PROFILER="rocprofv3 --att --att-activity 8 --"
 PROFILER=
 items_per_second="$($PROFILER $HOME/iree-build/tools/iree-benchmark-module --benchmark_min_warmup_time=1 --batch_size=1 --benchmark_min_time=1s \
   --benchmark_format=json \
@@ -51,18 +57,18 @@ items_per_second="$($PROFILER $HOME/iree-build/tools/iree-benchmark-module --ben
   --device_allocator=caching \
   --module=tmp/dispatch.vmfb \
   --function=matmul \
-  --input=${M}x${K}xf8E4M3FNUZ=@lhs.bin \
-  --input=${N}x${K}xf8E4M3FNUZ=@rhs.bin \
+  --input=${M}x${K}x${TYPE}=@lhs.bin \
+  --input=${N}x${K}x${TYPE}=@rhs.bin \
   | grep '"items_per_second":' \
   | cut -d ':' -f 2)"
 
 echo "print('%.1f Tflop/s' % (${items_per_second} * ${M} * ${N} * ${K} * 2e-12))" | python
 
 
-# $HOME/iree-build/tools/iree-benchmark-module --benchmark_min_warmup_time=1 --benchmark_repetitions=3 --batch_size=1  --benchmark_min_time=1s \
-#   --device=hip \
-#   --device_allocator=caching \
-#   --module=tmp/dispatch.vmfb \
-#   --function=matmul \
-#   --input=${M}x${K}xf8E4M3FNUZ=@lhs.bin \
-#   --input=${N}x${K}xf8E4M3FNUZ=@rhs.bin 
+$HOME/iree-build/tools/iree-benchmark-module --benchmark_min_warmup_time=1 --benchmark_repetitions=3 --batch_size=1  --benchmark_min_time=1s \
+  --device=hip \
+  --device_allocator=caching \
+  --module=tmp/dispatch.vmfb \
+  --function=matmul \
+  --input=${M}x${K}x${TYPE}=@lhs.bin \
+  --input=${N}x${K}x${TYPE}=@rhs.bin 
