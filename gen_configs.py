316801b46112d8185d1f45f7aeca0d8abb60f95f
Seb Vince <sebvince@amd.com>
2025-10-02 10:21:14 +0000

    start refactoring

--- gen_configs.py
+++ gen_configs.py
@@ -1,12 +1,11 @@
 import csv
 import os
 import sys
+import argparse
 from generate_solution import generate_files
 from rocprofResultsParser import parseJsonResults,parseCsvResults
 from single_test import compile,run
 
-# M = [2048,4096,4352,8192,8448,16384,16640,32768,33024,65536,65792,128256,131072]
-# N = [2048,4096,4352,8192,8448,16384,16640,32768,33024,65536,65792,128256,131072]
 M=[2048,4096,4608,4864,5376,7680,7936,8192,8448,8704,14080,14336,16128,16384,16640,32512,32768,33024,65536,65792,66048,128256,130816,131072]
 N=[2048,4096,4608,4864,5376,7680,7936,8192,8448,8704,14080,14336,16128,16384,16640,32512,32768,33024,65536,65792,66048,128256,130816,131072]
 
@@ -37,39 +36,103 @@ def getLastId(fileName):
             writer.writerow(['ID','M', 'N', 'K', 'TCC_HIT_RATE', 'time_ns','TCC_EA0_RDREQ','EA0_RDREQ_LEVEL'])
     return last_id
 
-fileName = 'summary'
+def updateRow(fileName, new_row):
+    hasInserted = False
+    rows = []
+    if os.path.exists(fileName):
+        with open(fileName, 'r', newline='') as f:
+            reader = csv.reader(f)
+            header = next(reader)
+            for row in reader:
+                # Check if this row matches M, N, K
+                if int(row[0]) == new_row[0] and int(row[1]) == new_row[1] and int(row[2]) == new_row[2]:
+                    rows.append(new_row)
+                    hasInserted = True
+                else:
+                    rows.append(row)
+
+    if not hasInserted:
+        rows.append(new_row)
+
+    # Write the updated rows back to the file
+    with open(fileName, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(['M', 'N', 'K', 'TCC_HIT_RATE', 'time_ns','TCC_EA0_RDREQ','EA0_RDREQ_LEVEL'])
+        writer.writerows(rows)
+        
+
+# fileName = 'summary'
 
 # Options
-# dtype='f16'
-isStatic = sys.argv[1]=='1'
-transposedReorder = sys.argv[2]=='1'
-dtype = sys.argv[3]
-
-print("isStatic:", isStatic)
-print("transposedReorder:", transposedReorder)
-
-fileName = fileName + '_static' if isStatic else fileName + '_dynamic'
-fileName = fileName + '_transposed' if transposedReorder else fileName + '_regular'
-fileName += f'_{dtype}'
-fileName += '.csv'
-startId = getLastId(fileName)+1
-print("Output to " + fileName)
-
-data = []
-nb_tests = len(test_cases)
 
 
-for index in range(startId,nb_tests):
-    (m,n,k) = test_cases[index]
-    print(f'Matmul_{m}_{n}_{k} : {index}')
-    generate_files(m,n,k,dtype,isStatic)
-    compile(transposedReorder)
-    run(m,n,k,dtype)
-    (median_TCC_HIT_RATE,median_time_ns,median_TCC_EA0_RDREQ , median_EA0_RDREQ_LEVEL) = parseCsvResults('res.csv_counter_collection.csv')
+def main():
+    parser = argparse.ArgumentParser(description="Process some integers.")
+    parser.add_argument('--startId', type=int, required=False, default= 0, help='Start id for benchmark')
+    parser.add_argument('--dynamic', action='store_true', help='Enable dynamic blocked shapes')
+    parser.add_argument('--dtype', type=str, required=False, default="f16", help='Data type')
+    args = parser.parse_args()
+
     
-    with open(fileName, 'a', newline='') as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow([index,m, n, k, median_TCC_HIT_RATE, median_time_ns,median_TCC_EA0_RDREQ,median_EA0_RDREQ_LEVEL])
+    tileSize = 256
+    isStatic = not args.dynamic
+    print(f"StartId: {args.startId}")
+    print(f"Dynamic: {args.dynamic}")
+    print(f"dtype: {args.dtype}")
+
+    fileName = 'summary'
+    fileName = fileName + '_static' if not args.dynamic else fileName + '_dynamic'
+    fileName += f'_{args.dtype}'
+    fileName += '.csv'
+    # startId = getLastId(fileName) + 1
+    print("Output to " + fileName)
+
+    nb_tests = len(test_cases)
+    # new_row = (131072,16640,4096,0.41144592064316166,31486707.5,653892473.0,236325697624.0)
+    # updateRow("test.csv", new_row)
+
+    # exit()
+    for index in range(args.startId,nb_tests):
+        (m,n,k) = test_cases[index]
+        print(f'Matmul_{m}_{n}_{k} : {index}')
+        generate_files(m,n,k,args.dtype,isStatic,tileSize)
+        compile()
+        run(m,n,k,args.dtype)
+        # (median_TCC_HIT_RATE,median_time_ns,median_TCC_EA0_RDREQ , median_EA0_RDREQ_LEVEL) = parseCsvResults('res.csv_counter_collection.csv')
+        new_row = parseCsvResults('res.csv_counter_collection.csv')
+        updateRow(fileName, (m,n,k) + new_row)
+    # with open(fileName, 'a', newline='') as csvfile:
+    #     writer = csv.writer(csvfile)
+    #     writer.writerow([index,m, n, k, median_TCC_HIT_RATE, median_time_ns,median_TCC_EA0_RDREQ,median_EA0_RDREQ_LEVEL])
+    
+
+if __name__ == "__main__":
+    main()
+
+# isStatic = sys.argv[1]=='1'
+# dtype = sys.argv[2]
+
+# print("isStatic:", isStatic)
+
+# fileName = fileName + '_static' if isStatic else fileName + '_dynamic'
+# fileName += f'_{dtype}'
+# fileName += '.csv'
+# startId = getLastId(fileName)+1
+# print("Output to " + fileName)
+
+
+
+# for index in range(startId,nb_tests):
+#     (m,n,k) = test_cases[index]
+#     print(f'Matmul_{m}_{n}_{k} : {index}')
+#     generate_files(m,n,k,dtype,isStatic,tileSize)
+#     compile(transposedReorder)
+#     run(m,n,k,dtype)
+#     (median_TCC_HIT_RATE,median_time_ns,median_TCC_EA0_RDREQ , median_EA0_RDREQ_LEVEL) = parseCsvResults('res.csv_counter_collection.csv')
+    
+#     with open(fileName, 'a', newline='') as csvfile:
+#         writer = csv.writer(csvfile)
+#         writer.writerow([index,m, n, k, median_TCC_HIT_RATE, median_time_ns,median_TCC_EA0_RDREQ,median_EA0_RDREQ_LEVEL])
 
 
 
--- single_test.py
+++ single_test.py
@@ -6,16 +6,14 @@ from rocprofResultsParser import parseJsonResults,parseCsvResults
 home_dir = os.path.expanduser("~")
 IREE_PATH=f'{home_dir}/iree-build/tools'
 
-def compile(useTranspose = False):
-    transposeStrategy = 'transpose' if useTranspose else 'none'
+def compile():
     cmd = [f'{IREE_PATH}/iree-compile',
     'matmul.mlir',
     '--iree-hip-target=gfx942',
     '--iree-hal-target-backends=rocm',
     '--iree-codegen-enable-default-tuning-specs=true',
     '--iree-hip-enable-tensor-ukernels',
-    '--iree-hal-dump-executable-files-to=files',
-    f'--iree-codegen-reorder-workgroups-strategy={transposeStrategy}',
+    # '--iree-hal-dump-executable-files-to=files',
     '--iree-opt-level=O3',
     '-o','tmp/dispatch.vmfb']
     print(" ".join(cmd))
@@ -55,14 +53,25 @@ def run(M,N,K,TYPE, profilePerXCD = False):
    
 if __name__ == "__main__":  
 
+# | 4608 | 16384 | 4096 | -36.6 % | 🔴 8.6 % | 898768.5 | 975723.0 | 634 | NEW: 0.9275725 / OLD :
+# | 4864 | 14336 | 4096 | -60.5 % | 🔴 8.3 % | 915093.0 | 990685.5 | 577 | NEW: 0.9511265 / OLD :
+# | 4864 | 16384 | 4096 | -59.6 % | 🔴 15.3 % | 909304.0 | 1048391.0 | 623 | NEW : 0.997273 / OLD :
+# | 5376 | 14336 | 4096 | -60.2 % | 🔴 14.9 % | 891237.5 | 1023795.0 | 617 | NEW : 0.9694135 / OLD :
+# | 5376 | 16384 | 4096 | -56.3 % | 🔴 8.9 % | 1136680.0 | 1237730.5 | 583 | NEW : 1.1952475 / OLD :
+# | 7680 | 14336 | 4096 | -39.9 % | 🔴 9.4 % | 1356246.0 | 1483438.5 | 608 | NEW : 1.413891 / OLD :
+# | 7936 | 14336 | 4096 | -53.0 % | 🔴 11.6 % | 1342705.0 | 1499120.5 | 622 | NEW : 1.495411 / OLD :
+# | 7936 | 16384 | 4096 | -49.3 % | 🔴 12.1 % | 1577752.5 | 1768860.0 | 602 | NEW : 1.7161985 / OLD :
+# | 8448 | 14336 | 4096 | -51.5 % | 🔴 10.3 % | 1556081.5 | 1716822.0 | 578 | NEW : 1.679323 / OLD :
+# | 8448 | 16384 | 4096 | -48.5 % | 🔴 12.9 % | 1610863.0 | 1819174.0 | 623 | NEW : 1.7714185 / OLD : 1.782656
+# | 8704 | 14336 | 4096 | -45.2 % | 🔴 10.4 % | 1566718.0 | 1730161.0 | 591 | NEW : 1.641787 / OLD :1.740531
 
-    M=8192
-    N=128256
+    M=4864
+    N=16384
     K=4096
-    tileSize = 128
+    tileSize = 256
     dtype='f16'#'f8E4M3FNUZ'
-    dtype='f8E4M3FNUZ'
-    isStatic = False
+    # dtype='f8E4M3FNUZ'
+    isStatic = True
     profilePerXCD = False
     transposedReorder = False
     filename ='res.csv_counter_collection.csv'
